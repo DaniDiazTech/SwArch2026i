77ff04354c93ad469e14508e45ed7c8ddba19f18
Daniel Diaz <danieldiazgonza52@gmail.com>
2026-02-15 11:57:19 -0500

    Added final lab structure

--- Laboratory-1-Architectural-Design/swarch/repositories/grade_repository.py
+++ Laboratory-1-Architectural-Design/swarch/repositories/grade_repository.py
@@ -1,6 +1,8 @@
 from models.grade import Grade, db
+
 def get_all():
  return Grade.query.all()
+
 def add(student_name, subject, score):
  new_grade = Grade(student_name=student_name, subject=subject, score=score)
  db.session.add(new_grade)
@@ -9,6 +11,6 @@ def add(student_name, subject, score):
 def delete_by_id(grade_id):
  grade = Grade.query.get(grade_id)
  if grade:
- db.session.delete(grade)
- db.session.commit()
+     db.session.delete(grade)
+     db.session.commit()
 
